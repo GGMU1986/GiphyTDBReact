9e2c026147cd45991470425229b86471b9a334e6
ggmu1986 <gt2186@gmail.com>
2022-08-03 12:55:43 -0400

    Added useState and utils file

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+  const [search, setSearch] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+
   return (
     <div className="header">
-      <div className="search">
-        <input type="text" placeholder="Search"/>
-        <button className="search_btn">Go</button>
-      </div>
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          className="search_input" 
+          type="text"
+          value={search} 
+          placeholder="Search for a GIF"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <input 
+          className="search_btn"
+          type="submit"
+          value="Go"
+        />
+      </form>
     </div>
   )
 }
