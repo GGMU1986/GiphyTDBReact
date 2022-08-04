5f85c17a1209ebcf1df1561543f81fbe202f9bca
ggmu1986 <gt2186@gmail.com>
2022-08-04 16:16:14 -0400

    Added masonry style with dependency

--- src/components/gifContainer.jsx
+++ src/components/gifContainer.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import uuid from 'react-uuid'
 import GifItem from './gifItem';
+import Masonry from 'react-masonry-css';
 
 const GifContainer = ({ gifs }) => {
  
   if (gifs){
     return (
-      <div className="gifs_container">
+      <Masonry
+        breakpointCols={4}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {gifs.map(gif => <GifItem key={uuid()} gif={gif} />)}
-      </div>
+      </Masonry>
     )
   } 
 }
